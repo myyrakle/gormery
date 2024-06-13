6e6151e150dca20570904249ca19608a2f791e64
myyrakle <sssang97@naver.com>
2024-06-14 00:38:02 +0900

    cobra CLI 세팅

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gormery",
+	Short: "generate gormery codes",
+	Run: func(cmd *cobra.Command, args []string) {
+		//run.RunGenerate()
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
