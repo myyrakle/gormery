157890a8359a9fb129a56149fdbca969195c25a1
myyrakle <sssang97@naver.com>
2024-06-17 23:35:19 +0900

    [#1] BaseImport config 제거

--- internal/config/config_file.go
+++ internal/config/config_file.go
@@ -2,7 +2,6 @@ package config
 
 type ConfigFile struct {
 	Basedir      string `yaml:"basedir"`       // 기본 경로 (ex: domain)
-	BaseImport   string `yaml:"base-import"`   // 기본 import (ex: github.com/myyrakle/gormery/example)
 	OutputSuffix string `yaml:"output-suffix"` // 출력 파일 접미사 (ex: _gorm.go)
 	RunnerPath   string `yaml:"runner-path"`   // 실행시킬 파일 경로
 
