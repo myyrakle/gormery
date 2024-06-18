8c64fb2dc2da5842ae1ac60b82e35f782628a8be
myyrakle <sssang97@naver.com>
2024-06-19 00:20:35 +0900

    [#5] config에 features 플래그 추가

--- internal/config/config_file.go
+++ internal/config/config_file.go
@@ -1,9 +1,10 @@
 package config
 
 type ConfigFile struct {
-	Basedir      string `yaml:"basedir"`       // 기본 경로 (ex: domain)
-	OutputSuffix string `yaml:"output-suffix"` // 출력 파일 접미사 (ex: _gorm.go)
-	RunnerPath   string `yaml:"runner-path"`   // 실행시킬 파일 경로
+	Basedir      string   `yaml:"basedir"`       // 기본 경로 (ex: domain)
+	OutputSuffix string   `yaml:"output-suffix"` // 출력 파일 접미사 (ex: _gorm.go)
+	RunnerPath   string   `yaml:"runner-path"`   // 실행시킬 파일 경로
+	Features     []string `yaml:"features"`      // 사용할 기능 목록
 
 	// internal config
 	ModuleName string // 프로젝트 모듈명 (go.mod에서 파싱)
